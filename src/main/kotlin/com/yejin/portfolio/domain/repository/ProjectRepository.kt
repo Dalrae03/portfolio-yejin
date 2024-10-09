54bb1ab901830d38d0b16660e90031e6d27206a8
Yejin Lee <www298@naver.com>
2024-10-09 15:13:37 +0900

    리포지토리 성능 개선

--- src/main/kotlin/com/yejin/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/com/yejin/portfolio/domain/repository/ProjectRepository.kt
@@ -7,6 +7,15 @@ import java.util.*
 
 interface ProjectRepository : JpaRepository<Project, Long> {
 
+    //jpa 1+n 문제
+    //-> 처음에 한 번 부모 데이터를 조회하고, 그래서 size는 10이었는데, 이 10개에 대해서 각각 자식을 또 가지고와야하니까 추가로 10번을 데이터베이스에 더 왔다갔다함
+
+    //ExperienceRepository의 OneToMany에서 fetch를 eager로 바꾸면, 한번에 11개의 쿼리를 보내고 가지고와서 데이터를 넣어둔 다음에 계속 순차적으로 찍는다.
+
+    //데이터를 넣어둔 다음에 계속 순차적으로 찍는다. => 가능한 이유: jpa에서 proxy를 쓰기 때문
+
+    //proxy: 가짜 객체
+
     //fach join의 단점 - 여러개의 엔티티와 관계를 맺고 있을 때 그것들을 다 한꺼번에 조회 불가
     //지금 이건 skill만 가지고 오지 detail은 가지고오지 못했기 때문에 디테일 호출할때마다 레이즈 때문에 매번가지고오게된다.
     //이런경우 네이티브 쿼리로 따로 풀어주거나, 쿼리 DSL이나 다른 방식으로 풀어주어야한다.
